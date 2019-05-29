0ed5cc4fea458f50c10f80db1e91624863a6447f
ANNA-PC\User <anana96koz@gmail.com>
2019-05-29 11:50:13 +0300

    Разработка 1.3.Добавление redux_revtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
-
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default function configureStore() {
-  const store = createStore(reducers, applyMiddleware(thunk),
- );
+  const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
   return store;
 }
